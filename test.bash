bdcfef118a5c8e20122a47b2c875c8151467ca4f
Takuma Ohsawa <s23c1025wd@s.chibakoudai.jp>
2024-11-07 10:55:10 +0900

    Revert test.bash to original state

---
+++ test.bash
@@ -0,0 +1,39 @@
+#!/bin/bash -xv
+
+ng () {
+	echo ${1}行目が違うヨ
+	res=1
+}
+
+res=0
+
+out=$(seq 5 | ./plus)
+++ seq 5
+++ ./plus
++ out=15
+[ "${out}" = 15 ] || ng "$LINENO"
++ '[' 15 = 15 ']'
+
+[ "${res}" = 0 ] && echo OK
++ `[` 0 = 0 `]`
++ echo OK
+OK
+exit $res
++ exit 0
+
+### NORMAL INPUT ###
+out=$(seq 5 | ./plus)
+[ "${out}" = 15 ] || ng "$LINENO"
+  　
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)           #計算できない値を入力してみる
+[ "$?" = 1 ]      || ng "$LINENO" #終了ステータスが1なのを確認
+[ "${out}" = "" ] || ng "$LINENO" #この行と上の行は入れ替えるとダメです
+  　                                      #（↑なぜかは考えてみましょう）
+out=$(echo | ./plus)              #なにも入力しない
+[ "$?" = 1 ]      || ng "$LINENO" #これも異常終了する
+[ "${out}" = "" ] || ng "$LINENO"
+  　
+[ "$res" = 0 ] && echo OK
+exit $res
+
