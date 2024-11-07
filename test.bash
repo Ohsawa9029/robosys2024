28ad21c6483fd4507100d012e844894cfbde78a4
Takuma Ohsawa <s23c1025wd@s.chibakoudai.jp>
2024-11-07 12:07:13 +0900

    Revert test.bash to original state

--- test.bash
+++ test.bash
@@ -8,18 +8,11 @@ ng () {
 res=0
 
 out=$(seq 5 | ./plus)
-++ seq 5
-++ ./plus
-+ out=15
 [ "${out}" = 15 ] || ng "$LINENO"
-+ '[' 15 = 15 ']'
 
 [ "${res}" = 0 ] && echo OK
-+ `[` 0 = 0 `]`
-+ echo OK
 OK
 exit $res
-+ exit 0
 
 ### NORMAL INPUT ###
 out=$(seq 5 | ./plus)
